74f12dc038b30d69ed43e03081914f8c2b5dfc04
ANRazvan <alexe.razvan05@gmail.com>
2025-04-02 12:42:11 +0300

    deleted .gitignore ftm

---
+++ ArtAttack/Shared/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ArtAttack.Shared
+{
+    public class Configuration
+    {
+
+        public static string _CONNECTION_STRING_ = "Data Source=RAZVAN\\SQLEXPRESS01;Initial Catalog=PurchaseDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    }
+}
